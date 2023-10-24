dfc533aed4fa2c01a7f40d9f4b2eadce355f289e
thom-window <hsbang.thom@gmail.com>
2023-10-24 23:16:33 +0900

    annotation processor with source builder

--- src/main/java/org/thom/BoxProcessor.java
+++ src/main/java/org/thom/BoxProcessor.java
@@ -1,17 +1,20 @@
 package org.thom;
 
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 import com.sun.source.tree.Tree;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.Name;
-import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.*;
 import javax.tools.Diagnostic;
+import java.io.IOException;
 import java.util.Set;
 
 @AutoService(Processor.class)
@@ -39,6 +42,33 @@ public class BoxProcessor extends AbstractProcessor {
             } else {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "Processing " + element);
             }
+
+            TypeElement typeElement = (TypeElement) element;
+            ClassName className = ClassName.get(typeElement);
+
+            System.out.println("className = " + className);
+
+            MethodSpec pullOutMethod = MethodSpec.methodBuilder("pullOut")
+                    .addModifiers(Modifier.PUBLIC)
+                    .returns(String.class)
+                    .addStatement("return $S", "another box!")
+                    .build();
+
+            TypeSpec box = TypeSpec.classBuilder("GloryBox")
+                    .addModifiers(Modifier.PUBLIC)
+                    .addSuperinterface(className)
+                    .addMethod(pullOutMethod)
+                    .build();
+
+            Filer filer = processingEnv.getFiler();
+
+            try {
+                JavaFile.builder(className.packageName(), box).build().writeTo(filer);
+            } catch (IOException e) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "fata error" + e.getMessage());
+            }
+
+
         }
         return true;
     }
